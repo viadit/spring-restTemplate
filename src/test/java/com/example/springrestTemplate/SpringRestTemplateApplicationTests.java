9066d57c127743bff340d11bdf76245a9f38c568
Vishal <vishal.aditya6@gmail.com>
2021-10-31 20:51:52 +0530

    Initial Commit

---
+++ src/test/java/com/example/springrestTemplate/SpringRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
